2c8833b9cc50924c890a32206e2834adbab33d73
aaaaaghhhhh <3028209478@qq.com>
2023-11-06 04:35:18 +0800

    maven 11.6

---
+++ SnakeGame-master/src/main/java/com/example/snakegame/GameFrame.java
@@ -0,0 +1,22 @@
+package com.example.snakegame;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class GameFrame extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Pane root = new Pane();
+        Scene scene = new Scene(root, 600, 400); // Replace these dimensions with your game's dimensions
+        primaryStage.setTitle("Snake Game");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ SnakeGame-master/src/main/java/com/example/snakegame/SnakeGameSRC.java
@@ -0,0 +1,12 @@
+package com.example.snakegame;
+
+import com.example.snakegame.GameFrame;
+import javafx.application.Application;
+
+public class SnakeGameSRC {
+    public static void main(String[] args) {
+        // JavaFX 应用程序的启动方式
+        Application.launch(GameFrame.class, args);
+    }
+}
+
